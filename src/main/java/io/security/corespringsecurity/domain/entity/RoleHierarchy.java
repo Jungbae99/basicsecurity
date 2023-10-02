17c67879e8511defd276c7dbf1deca2e7d58a8cf
jungbaejeon <wjdqotktls55@gmail.com>
2023-10-02 16:09:37 +0900

    Feat/ 역할 상속 구현
    1. 엔티티로 만들어 역할을 DB에 저장합니다.
    
    2. 인가 프로세스를 단순화하고 하위 역할을 자동으로 부여받게 됩니다 (admin, manager, user -> admin)

---
+++ src/main/java/io/security/corespringsecurity/domain/entity/RoleHierarchy.java
@@ -0,0 +1,35 @@
+package io.security.corespringsecurity.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="ROLE_HIERARCHY")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString(exclude = {"parentName", "roleHierarchy"})
+//@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class)
+public class RoleHierarchy implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "child_name")
+    private String childName;
+
+    @ManyToOne(cascade = {CascadeType.ALL},fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_name", referencedColumnName = "child_name")
+    private RoleHierarchy parentName;
+
+    @OneToMany(mappedBy = "parentName", cascade={CascadeType.ALL})
+    private Set<RoleHierarchy> roleHierarchy = new HashSet<RoleHierarchy>();
+}
---
+++ src/main/java/io/security/corespringsecurity/security/service/RoleHierarchyService.java
@@ -0,0 +1,6 @@
+package io.security.corespringsecurity.security.service;
+
+public interface RoleHierarchyService {
+
+    String findAllHierarchy();
+}
